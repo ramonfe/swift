20e847a6aa01c1f736c21a9c565a86b9c497d3dc
Ramon Felix <ramonfe@gmail.com>
2020-03-15 19:46:13 -0700

    hello world

---
+++ Evaluacion1.playground/Contents.swift
@@ -0,0 +1,20 @@
+//Creado Por: Ramon Felix. Marzo 15 2020.
+import UIKit
+
+var rango = 0...100
+
+for i in rango{
+    if i%5 == 0
+    {
+    print("\(i)\tBingo!!!")
+    }
+    else if i%2==0 {
+        print("\(i)\tPar!!!")
+    }
+    else if i%2>0{
+        print("\(i)\tInPar!!!")
+    }
+    if i>=30 && i<=40{
+        print("\(i)\tViva Swift!!!")
+    }
+}
