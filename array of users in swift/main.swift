13f88efd2c6a1fb81d473ba66ad28e2ed6916237
ramonfe <ramonfe@gmail.com>
2022-02-10 21:29:23 -0800

    Initial Commit

---
+++ array of users in swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  array of users in swift
+//
+//  Created by home on 10/02/22.
+//  Copyright © 2022 RF. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
