2f9ca51f920b8380cb08f95bb0220d07bdb39574
ramonfe <ramonfe@gmail.com>
2022-02-17 21:52:01 -0800

    finish class func, is like static in other languages

---
+++ class func swift/class func swift/main.swift
@@ -0,0 +1,14 @@
+
+import Foundation
+
+class myClass{
+    func myFunc(){
+        
+    }
+    class func funcLikeStatic(){}
+}
+let myclass = myClass()
+myclass.myFunc()
+
+//is like methods static in C#
+myClass.funcLikeStatic()
