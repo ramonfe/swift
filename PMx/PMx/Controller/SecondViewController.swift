3e962dfd8ec942c2519f435ae587d5fab4eaad69
ramonfe <ramonfe@gmail.com>
2022-05-08 21:02:07 -0700

    adding vary for traits for banner adding footer table to avoid being covered by the banner adding responsive text size to launch text depending of screen size

--- PMx/PMx/Controller/SecondViewController.swift
+++ PMx/PMx/Controller/SecondViewController.swift
@@ -21,15 +21,54 @@ class SecondViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        loadBanner()
+        bannerView.adUnitID = "ca-app-pub-3940256099942544/2934735716"//"ca-app-pub-6687064215304616/1359059372"
+        bannerView.rootViewController = self//ca-app-pub-3940256099942544/2934735716
+        
         sendFirebaseLog()
         showSpinner()
+        
         myTableView.dataSource=self
         myTableView.delegate=self
         myTableView.tableFooterView = UIView()
         myTableView.register(UINib(nibName: "MyTableViewCell", bundle: nil), forCellReuseIdentifier: "mycustomcell")
         
         loadJsonInTable()
+    }
+    override func viewWillLayoutSubviews() {
+        //para que el banner no cubra el contenido de la tabla
+        myTableView.tableFooterView = UIView(frame: CGRect(x: 0, y: 0, width: 1, height: 100))
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        loadBannerAd()
+    }
+    override func viewWillTransition(to size: CGSize,
+                                     with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to:size, with:coordinator)
+        coordinator.animate(alongsideTransition: { _ in
+            self.loadBannerAd()
+        })
+    }
+    func loadBannerAd(){
+        // Step 2 - Determine the view width to use for the ad width.
+        let frame = { () -> CGRect in
+            // Here safe area is taken into account, hence the view frame is used
+            // after the view has been laid out.
+            if #available(iOS 11.0, *) {
+                return view.frame.inset(by: view.safeAreaInsets)
+            } else {
+                return view.frame
+            }
+        }()
+        let viewWidth = frame.size.width
+        
+        // Step 3 - Get Adaptive GADAdSize and set the ad view.
+        // Here the current interface orientation is used. If the ad is being preloaded
+        // for a future orientation change or different orientation, the function for the
+        // relevant orientation should be used.
+        bannerView.adSize = GADCurrentOrientationAnchoredAdaptiveBannerAdSizeWithWidth(viewWidth)
+        // Step 4 - Create an ad request and load the adaptive banner ad.
+        bannerView.load(GADRequest())
     }
     func sendFirebaseLog(){
         // Analu¡ytic event.
@@ -48,13 +87,6 @@ class SecondViewController: UIViewController {
             self.hideSpinner()           
         }
     }
-    func loadBanner(){
-        bannerView.adUnitID = "ca-app-pub-3940256099942544/2934735716"//"ca-app-pub-6687064215304616/1359059372"
-        bannerView.rootViewController = self//ca-app-pub-3940256099942544/2934735716
-        bannerView.load(GADRequest())
-        
-        bannerView.delegate = self
-    }
     func showSpinner(){
         activityIndicator.startAnimating()
         activityIndicator.isHidden=false
@@ -62,32 +94,6 @@ class SecondViewController: UIViewController {
     func hideSpinner(){
         activityIndicator.stopAnimating()
         activityIndicator.isHidden=true
-    }
-}
-//MARK: GADBannerViewDelegate
-extension SecondViewController:GADBannerViewDelegate{
-    func bannerViewDidReceiveAd(_ bannerView: GADBannerView) {
-        print("bannerViewDidReceiveAd")
-    }
-    
-    func bannerView(_ bannerView: GADBannerView, didFailToReceiveAdWithError error: Error) {
-        print("bannerView:didFailToReceiveAdWithError: \(error.localizedDescription)")
-    }
-    
-    func bannerViewDidRecordImpression(_ bannerView: GADBannerView) {
-        print("bannerViewDidRecordImpression")
-    }
-    
-    func bannerViewWillPresentScreen(_ bannerView: GADBannerView) {
-        print("bannerViewWillPresentScreen")
-    }
-    
-    func bannerViewWillDismissScreen(_ bannerView: GADBannerView) {
-        print("bannerViewWillDIsmissScreen")
-    }
-    
-    func bannerViewDidDismissScreen(_ bannerView: GADBannerView) {
-        print("bannerViewDidDismissScreen")
     }
 }
 //MARK: UITableViewDelegate
