d429eba596122b2894f03b543878ca9f9de35144
ramonfe-mini <ramonfe@gmail.com>
2022-09-19 20:22:09 -0700

    changes in yelp study

--- Training/Playgrounds/YelpStudyGuide.playground/Contents.swift
+++ Training/Playgrounds/YelpStudyGuide.playground/Contents.swift
@@ -1,7 +1,13 @@
 import UIKit
-
+/*
+ Given a list of businesses and a desired search text find the first 3 businesses prefix-matching the searched phrase (on any of the words)
+ */
 let places = ["Burgers", "Papas Burger", "Pizza Italiana"]
-
 let findPlaces = places.filter{ $0.localizedCaseInsensitiveContains ("bur") }
-
 print(findPlaces.prefix(5))
+
+//remove extra spaces from string
+let stringWspaces = "string with spaces at end         "
+
+print(stringWspaces.trimmingCharacters(in: .whitespaces))
+
